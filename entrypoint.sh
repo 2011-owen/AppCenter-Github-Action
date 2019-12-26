8707ef6044895b1f3ba23d351a16adc1055fbe11
Zach Silveira <zackify@gmail.com>
2019-12-26 12:21:20 -0800

    Use commit message for release notes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group "$INPUT_GROUP" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug
+appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group "$INPUT_GROUP" --file "$INPUT_FILE" --release-notes "$(git log -1 --pretty=format:%s)" --debug
 
